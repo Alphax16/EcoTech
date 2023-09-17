13161bdc09b3970c14de0b81681dba5ae2512c0a
Alphax <90373222+Alphax16@users.noreply.github.com>
2023-09-17 07:00:05 +0530

    Update WaterPotabilityPredictor.py

--- backend/python-models/WaterPotabilityPredictor.py
+++ backend/python-models/WaterPotabilityPredictor.py
@@ -10,5 +10,30 @@ warnings.filterwarnings('ignore')
 current_directory = os.getcwd()
 items = os.listdir(current_directory)
 items_string = ', '.join(items)
-print(items_string)
+# print(items_string)
+
+with open("./weights-pickles/WaterPotability_Final.pkl", 'rb') as fpk:
+    model = load(fpk)
+
+# print(model)
+
+ph = 7.5
+Hardness = 145
+Solids = 19909
+Chloramines = 7
+Sulfate = 350
+Conductivity = 500
+Organic_carbon = 14
+Trihalomethanes = 50
+Turbidity = 4
+
+# ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic_carbon, Trihalomethanes, Turbidity = argv[1:]
+
+X = np.array([[ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic_carbon, Trihalomethanes, Turbidity]])
+
+try:
+    X_pred = ['potable', 'not potable'][model.predict(X)[0]]
+    print(X_pred)
+except Exception as ex:
+    print(ex)
 
